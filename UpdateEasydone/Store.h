115d59915944928e3cfd4f3baa2eb76a029a8b3a
Daniel <Daniel@localhost>
2014-03-13 16:07:01 +0800

    Included edited worker and store

--- UpdateEasydone/Store.h
+++ UpdateEasydone/Store.h
@@ -7,21 +7,30 @@
 #include <stdio.h>
 #include <string>
 #include <vector>
+#include <iterator>
 
 using namespace std;
 
+/*typedef struct DateFormat {
+	string day;
+	string month;
+	string year;
+};*/
 
-typedef struct Task{
+
+typedef struct Task {
 	string taskName;
-	int index;	
-	int startDate;
-	int startTime;
-	int endDate;
-	int endTime;
-	int date;
-	int month;
-	int year;
+	string index;	
+	string startDate;
+	string startTime;
+	string endDate;
+	string endTime;
+	string date;
+	string month;
+	string year;
 };
+
+vector<Task>::iterator iter;
 
 class Store {
 	
@@ -32,14 +41,18 @@ class Store {
 	
 	public:
 		
-		int getIndex(vector<Task> taskList);
-		string getTaskName(int index);
-		string getStartDate(int index);
-		string getStartTime(int index);
-		string getEndDate(int index);
-		string getEndTime(int index);
-		string getSize();
+		//string getIndex(int index);
+		string getTaskName(int slotNumber);
+		string getStartDate(int slotNumber);
+		string getStartTime(int slotNumber);
+		string getEndDate(int slotNumber);
+		string getEndTime(int slotNumber);
+		int getSize();
 		Task accessSlot(int slot); 
+		vector<Task>::iterator getIteratorBegin();
+		vector<Task>::iterator getIteratorEnd();
+		bool eraser(vector<Task>::iterator iterPassed);
+
 	
 };
 
