5b4ca804c51470f1f14ece0f9015eb6e2449ba74
Jiajun <Jiajun@localhost>
2014-03-13 18:34:33 +0800

    Changed Manager: removed statement to check fileName, because no need anymore.

--- EasyDone/Manager.cpp
+++ EasyDone/Manager.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 const string WELCOME_MESSAGE = "Hi! Welcome to EasyDone!";
-const string USER_PROMPT = "What would you like to do today?";
+const string USER_PROMPT = "What would you like to do today?\n";
 const string KEYED_EXIT = "exit";
-const string FILE_FORMAT = ".txt";
+const string FILE_NAME = "storageFile.txt";
 
 Manager::Manager(void) {
 }
@@ -14,60 +14,41 @@ Manager::~Manager(void) {
 }
 
 void Manager::receiveInput(string input) {
-	_userInput = input;
+	userInput = input;
 }
 
 string Manager::getUserInput() {
-	return _userInput;
+	return userInput;
 }
 
 void Manager::init() {
  
 	bool ready = 0;
 	FileHandler Loader;
-	Parser ParserJob;
-	Worker WorkerJob;
+	Parser parserJob;
+	Worker workerJob;
 	bool continue_running = true;
 
-	fileName = "storageFile.txt";
+	fileName = FILE_NAME;
 
-//This condition checks if the filename input name contains the correct extension. 
-//If not, it by default appends the extension.
-
-	if (fileName.size() > 4 && fileName.substr(fileName.size()-4, fileName.size()) != FILE_FORMAT){
-		fileName = fileName + FILE_FORMAT;
-	}
-	else if (fileName.size() < 4){
-		fileName = fileName + FILE_FORMAT;
-	}
-
-
-	while(continue_running)
-	{
+	while(continue_running) {
 		ready = Loader.fileReady(fileName);
 
-		if(ready != 0){
-	
-			while (1)
-			{
+		if(ready != 0) {
+			while (1) {
 				cout << WELCOME_MESSAGE;
-				cout<< USER_PROMPT<< endl;
-
-				getline(cin, userInput);
-
+				cout << USER_PROMPT;
+				
 				//switch(userCommand)
 				if (userInput == KEYED_EXIT){
 
 					continue_running = false;
 					break;
 
-				Parser parserJob;
-				Worker workerJob;
+					vector<string> parsedInput  = parserJob.completeParse (userInput);
+					string jobReturn = workerJob.takeparsedCommand(parsedInput);
 
-				vector<string> parsedInput  = parserJob.completeParse (userInput);
-				string jobReturn = workerJob.takeparsedCommand(parsedInput);
-
-				cout << jobReturn;
+					cout << jobReturn;
 				}
 			}
 		}
