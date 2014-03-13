2f3710508ea046c9b8abb2973c95b7d90fb97f26
Jiajun <Jiajun@localhost>
2014-03-13 16:01:50 +0800

    Updated EasyDone library

--- EasyDone/Manager.h
+++ EasyDone/Manager.h
@@ -15,34 +15,104 @@ public:
 };
 
 /*
-#ifndef STORE_H
-#define STORE_H
-
-#include <iostream>
-#include <vector>
 #include <stdio.h>
+#include <iostream>
 #include <fstream>
 #include <sstream>
 #include <string>
-#include <cctype>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include "Parser.h"
+#include "Worker.h"
+#include "FileHandler.h"
 
 
 using namespace std;
 
-class FileHandler {
+int indexOfTask;
+string contentToBeUpdated;
+string userInput;
+string taskName;
+string additionalDetails;
+//string reminder;
+//string duplciate;		//whats a duplicate
+string startDate;
+string startTime;
+string endDate;
+string endTime;
+string isCompleted;		// '1': task completed '0':not completed
+vector<string> detailsFromParser;
+//bool hasTimeSlot;		//true if timeslot is available, false if timeslot is occupied
 
-private:
-	string line;
-	vector<string> sentencesVector;
-	static const string MESSAGE_OUTPUT_WELCOME;
-	static const string MESSAGE_READY_FOR_USE;
+string fileName;
 
-public:
-	void load(string inputText);
-	void remove();
-	bool fileReady(string fileName);
-	bool diskcopy(string fileName);
+string outputMessage;
+string KeyWord;
+vector<string> toBeDisplayed;
+string fieldToBeUpdated;
 
-};
+const string WELCOME_MESSAGE = "Hi! Welcome to EasyDone!";
+const string USER_PROMPT = "What would you like to do today?";
+const string KEYED_EXIT = "exit";
+const string FILE_FORMAT = ".txt";
 
-#endif */
+
+int main (int argc, char* argv[]) {
+ 
+	
+	bool ready = 0;
+	FileHandler Loader;
+	Parser ParserJob;
+	Worker WorkerJob;
+	bool continue_running;
+
+	fileName = argv[1];
+
+//This condition checks if the filename input name contains the correct extension. 
+//If not, it by default appends the extension.
+
+	if (fileName.size() > 4 && fileName.substr(fileName.size()-4, fileName.size()) != FILE_FORMAT){
+		fileName = fileName + FILE_FORMAT;
+	}
+	else if (fileName.size() < 4){
+		fileName = fileName + FILE_FORMAT;
+	}
+
+
+	while(continue_running)
+	{
+		ready = Loader.fileReady(fileName);
+
+		if(ready != 0){
+	
+			while (1)
+			{
+				cout << WELCOME_MESSAGE;
+				cout<< USER_PROMPT<< endl;
+
+				getline(cin, userInput);
+
+				//switch(userCommand)
+				if (userInput == KEYED_EXIT){
+
+					continue_running = false;
+					break;
+
+				Parser parserJob;
+				Worker workerJob;
+
+				vector<string> parsedInput  = parserJob.completeParse (userInput);
+				string jobReturn = workerJob.takeparsedCommand(parsedInput);
+
+				cout << jobReturn;
+				}
+			}
+		}
+	}
+	
+
+    return 0;
+	
+}
+*/
