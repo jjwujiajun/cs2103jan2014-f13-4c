af72d699ee53568a77a1078c288503ca9d3b9c77
Jiajun <Jiajun@localhost>
2014-03-13 16:18:38 +0800

    Updated easyDone lib

---
+++ EasyDone/Store.cpp
@@ -0,0 +1,46 @@
+#include "Store.h"
+
+
+/*
+string Store::getTaskName(int index) {
+	
+	string taskName1 = taskList[index].taskName;
+
+	return taskName1;
+}
+
+string Store::getStartTime(int index) {
+	string startTime1=  taskList[index].startTime;
+	
+	return startTime1;
+}
+
+string Store::getEndTime(int index) {
+	string endTime1=  taskList[index].startTime;
+	
+	return endTime1;
+}
+
+string Store::getStartDate(int index) {
+	string startDate1 = taskList[index].startDate;
+	
+	return startDate1;
+}
+
+string Store::getEndDate(int index) {
+	string getEndDate1 = taskList[index].endDate;
+	
+	return getEndDate1;
+}
+
+int Store::getSize() {
+	int sizeoftaskList = taskList.size();
+
+	return sizeoftaskList;
+}
+
+Task Store::accessSlot(int slot) {
+
+	return taskList.[slot];
+}
+*/
