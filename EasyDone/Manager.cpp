5bafd4e5dfe67289399d83bfcf593b4d5fdf8a89
Jiajun <Jiajun@localhost>
2014-03-14 15:32:42 +0800

    Update manager: remove couts

--- EasyDone/Manager.cpp
+++ EasyDone/Manager.cpp
@@ -29,24 +29,22 @@ void Manager::init() {
 	vector<string> parsedInput;
 	string feedback;
 
-	//bool isReady = false;
-	//isReady = fileHandler.fileReady(FILE_NAME);
+	/* FileHandler to be Deleted!
+	 * **********************************
+	 * bool isReady = false;
+	 * isReady = fileHandler.fileReady(FILE_NAME);
+	 *
+	 * add: do assert isReady here!
+	 * if(isReady) {
+	 * **********************************/
 
-	// add: do assert isReady here!
-	//if(isReady) {
 		// improve: return by pointer
-		
-
 		parsedInput  = parser.completeParse (userInput);
-		
-		for(int i=0; i<9; i++)
-			parsedInput.push_back("0");
-		//cout<<"hereeee"<<parsedInput[0]<< "	"<<parsedInput[1]<<endl;
 		feedback = worker.takeparsedCommand(parsedInput);
 		
-		//GUIfeedbackBox = feedback;
-		//GUIfeedbackBox += "What would you like to do today?\r\n";
-		//cout<<feedback<<endl;
+		GUIfeedbackBox = feedback;
+		GUIfeedbackBox += "What would you like to do today?\r\n";
+		
 		parser.parserEmpty();
 	//}
 }
