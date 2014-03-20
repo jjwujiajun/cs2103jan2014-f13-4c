caac2e88eb3f2a6c41f87f8bff4cae765d54ab90
Daniel <Daniel@localhost>
2014-03-20 17:47:50 +0800

    added command and task

---
+++ EasyDone/Task.h
@@ -0,0 +1,16 @@
+#include "Headers.h"
+
+using namespace std;
+
+class Task {
+public:
+	string taskName;
+	string taskID;	
+	string startDate;
+	string startTime;
+	string endDate;
+	string endTime;
+	string date;
+	string month;
+	string year;
+};
