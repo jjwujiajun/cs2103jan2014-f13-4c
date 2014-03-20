55526c83200cf5acd244f9e31607d49fd9040779
Jiajun <Jiajun@localhost>
2014-03-20 16:25:16 +0800

    Added: Log class in Headers.h. Now included in everybody's class.

---
+++ EasyDone/Log.h
@@ -0,0 +1,18 @@
+#include <fstream>
+#include <string>
+using namespace std;
+#pragma once
+
+class Log
+{
+private:
+	ofstream file;
+
+public:
+	Log(void);
+	~Log(void);
+
+	void clear();
+	void log(string);
+};
+
