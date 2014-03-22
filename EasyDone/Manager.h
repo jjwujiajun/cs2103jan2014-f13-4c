b8a8528474c9c4a03220ad34baa95271a399090e
Jiajun <Jiajun@localhost>
2014-03-22 16:54:38 +0800

    Maanager: added Hint to use F1 for help :)

--- EasyDone/Manager.h
+++ EasyDone/Manager.h
@@ -6,8 +6,8 @@
 using namespace std;
 
 const string FEEDBACK_MESSAGE_WELCOME = "Hi! Welcome to EasyDone!\r\n";
-const string FEEDBACK_PROMPT_START = "What would you like to do today?\r\n";
-const string FEEDBACK_PROMPT_OTHERS = "What else would you like to do? \r\n";
+const string FEEDBACK_PROMPT_START = "What would you like to do today?\r\n[Press F1 for Help.]";
+const string FEEDBACK_PROMPT_OTHERS = "What else would you like to do? \r\n[Press F1 for Help.]";
 const string LOG_MANAGER_TO_PARSER = "Manager: Pass userInput to parser";
 const string LOG_MANAGER_TO_WORKER = "Manager: Pass parsedInput to worker";
 const string LOG_STRING_USERINPUT = "User input: ";
