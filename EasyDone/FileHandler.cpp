fb6a0c17cef89db286abd531d4ec5efd37c3d1a6
arunrana1214 <arunrana1214@localhost>
2014-04-09 17:56:49 +0800

    added update guard

---
+++ EasyDone/FileHandler.cpp
@@ -0,0 +1,116 @@
+#include "FileHandler.h"
+
+using namespace std;
+
+vector<Task> FileHandler::getTaskList() {
+	string line;
+	bool isReadable;
+	Task task;
+	vector<Task> taskList;
+
+	readFile.open(FILE_TASKLIST);
+
+	while(getline(readFile, line)) {
+		task.taskID = line;
+		
+		isReadable = getline(readFile, line);
+		assert(isReadable == true);
+		task.taskName = line;
+		
+		isReadable = getline(readFile, line);
+		assert(isReadable == true);
+		task.startDate = line;
+		
+		isReadable = getline(readFile, line);
+		assert(isReadable == true);
+		task.startTime = line;
+		
+		isReadable = getline(readFile, line);
+		assert(isReadable == true);
+		task.endDate = line;
+		
+		isReadable = getline(readFile, line);
+		assert(isReadable == true);
+		task.endTime = line;
+
+		taskList.push_back(task);
+		getline(readFile, line);
+	}
+
+	readFile.close();
+
+	return taskList;
+}
+
+void FileHandler::saveTaskList(const vector<Task>& taskList) {
+	writeFile.open(FILE_TASKLIST, ios::trunc);
+
+	for (int i = 0; i < (int) taskList.size(); ++i) {
+		writeFile << taskList[i].taskID << endl;
+		writeFile << taskList[i].taskName << endl;
+		writeFile << taskList[i].startDate << endl;
+		writeFile << taskList[i].startTime << endl;
+		writeFile << taskList[i].endDate << endl;
+		writeFile << taskList[i].endTime << endl;
+		writeFile << endl;
+	}
+
+	writeFile.close();
+}
+
+bool FileHandler::getPresetSetting(const string& fileName) {
+	string fileStoredSetting;
+	ifstream file;
+	bool setting;
+
+	file.open(fileName);
+	if (getline(file, fileStoredSetting)) {
+		setting = fileStoredSetting == FILE_STRING_TRUE;
+		return setting;
+	}
+	return true;
+}
+
+themeColor FileHandler::getPresetTheme() {
+	ifstream presetTheme;
+	string fileStoredTheme;
+	themeColor theme;
+
+	presetTheme.open(FILE_SETTINGS_THEME);
+	getline(presetTheme, fileStoredTheme);
+
+	if (fileStoredTheme == FILE_STRING_BLUETHEME) {
+		theme = BLUE;
+	} else if (fileStoredTheme == FILE_STRING_METALTHEME) {
+		theme = METAL;
+	} else {
+		theme = WHITE;
+	}
+	presetTheme.close();
+	return theme;
+}
+
+void FileHandler::saveSettingForFile(const bool& isOn, const string& fileName) {
+	ofstream presetFile;
+	
+	presetFile.open(fileName, ios::trunc);
+	if (isOn) {
+		presetFile << FILE_STRING_TRUE;
+	} else {
+		presetFile << FILE_STRING_FALSE;
+	}
+}
+
+void FileHandler::saveTheme(const themeColor& theme) {
+	ofstream presetTheme;
+	string fileStoredTheme;
+	presetTheme.open(FILE_SETTINGS_THEME, ios::trunc);
+
+	if (theme == WHITE) {
+		presetTheme << FILE_STRING_WHITETHEME << endl;
+	} else if (theme == BLUE) {
+		presetTheme << FILE_STRING_BLUETHEME << endl;
+	} else if (theme == METAL) {
+		presetTheme << FILE_STRING_METALTHEME << endl;
+	}
+}
---
+++ EasyDone/Store.h
@@ -0,0 +1,76 @@
+#include "Headers.h"
+#include "FileHandler.h"
+
+
+using namespace std;
+
+#pragma once
+
+/*typedef struct DateFormat {
+	string day;
+	string month;
+	string year;
+};*/
+
+
+class Store {
+	
+	private:
+		FileHandler file;
+		vector<Task> taskList;
+		vector<Task> searchTask;
+		vector< vector<Task> > undoList;
+		void getDateTomorrow();
+		string tomorrowDay;
+		string tomorrowMonth;
+		string tomorrowYear;
+
+		Log log;
+	
+	
+	public:
+		Store();
+		~Store();
+
+		//string getIndex(int index);
+		string getTaskName(int slotNumber);
+		string getStartDate(int slotNumber);
+		string getStartTime(int slotNumber);
+		string getEndDate(int slotNumber);
+		string getEndTime(int slotNumber);
+		string getDay(int slotNumber);
+		string getMonth(int slotNumber);
+		string getYear(int slotNumber);
+		vector<Task> getTaskList();
+		vector<Task> getSearchedList();
+		int getSize();
+		string getTaskID(int slotNumber);
+
+
+		
+		Task accessSlot(int slot); 
+		vector<Task>::iterator getIteratorBegin();
+		vector<Task>::iterator getIteratorEnd();
+		void saveToFile();
+
+		bool MarkDone(Task taskID);  //arun pls make all helper functions private, only public functions are included in API!
+		Task accesswithTaskID(int indexEntered);
+		void switchTask(int slot1, int slot2);
+		void changeTask(int slot, Task slotTask);
+		bool eraser(string taskIndex);
+		void pushback(Task newTask);
+		Task getTask(int slot);
+		bool changeTask(int taskID, Task userTask, string updateField);
+		bool SearchItem(int Index, string searchField, string searchItem);
+		//int findSlot(string taskIndex);
+		void markTasksOverdue(); //bring this down!
+		bool stackToList();
+		void listToStack();
+		void updateTaskID();
+		void markTasksDueToday();
+		void markTasksDueTomorrow();
+		string currentDay();
+		string currentMonth();
+		string currentYear();
+	
+};
