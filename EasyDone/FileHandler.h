fdceaf81d5a8e633bfddb65a0c0122f760d60b40
Daniel <Daniel@localhost>
2014-04-11 10:25:21 +0800

    V0.4 parser: corrected leap year bugs. Added new NLP format fro "others"

---
+++ EasyDone/FileHandler.h
@@ -0,0 +1,25 @@
+#ifndef FILEHANDLER_H
+#define FILEHANDLER_H
+
+#include "Headers.h"
+
+using namespace std;
+
+class FileHandler {
+
+private:
+	ofstream writeFile;
+	ifstream readFile;
+
+public:
+	void createHelpHeadingInVector(vector<string>&);
+	void createHelpInstructionsInVector(vector<string>&);
+	vector<Task> getTaskList();
+	void saveTaskList(const vector<Task>&);
+	bool getPresetSetting(const string&);
+	themeColor getPresetTheme();
+	void saveSettingForFile(const bool&, const string&);
+	void saveTheme(const themeColor&);
+};
+
+#endif
